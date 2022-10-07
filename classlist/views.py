8b9aa089968f3ec4443ec38fa573fc6400f0fe05
schelimilla <srini.chelimilla@gmail.com>
2022-10-07 16:42:18 -0400

    Redirected to login page from home

--- classlist/views.py
+++ classlist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
 
 """
 Citations:
@@ -7,9 +8,7 @@ Title: Python + Django page redirect
 URL: https://stackoverflow.com/questions/523356/python-django-page-redirect
 """
 
-#Very basic index page created
-def index(request):
-    return HttpResponse("Hello, world. You're at the classlist index. Basic Django app created.")
+
 def view_name(request):
     # ex. http://127.0.0.1:8000/accounts/google/login/
     template_name = "classlist/google_login.html"
@@ -25,3 +24,7 @@ def view_name(request):
     
     # return render(request, 'google_login.html', context)
 
+def view_home(request):
+    template_name = "classlist/home.html"
+    return render(request, template_name)
+   
