ec81d0a1edba3bbb2ab637522f4db6ead3245331
megkuo <mlk6une@virginia.edu>
2022-10-07 14:26:41 -0400

    updated /login path to redirect to /accounts/google/login

--- classlist/views.py
+++ classlist/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+"""
+Citations:
+Title: Python + Django page redirect
+URL: https://stackoverflow.com/questions/523356/python-django-page-redirect
+"""
 
 #Very basic index page created
 def index(request):
     return HttpResponse("Hello, world. You're at the classlist index. Basic Django app created.")
 def view_name(request):
-    return render(request, 'google_login.html', {})
+    # ex. http://127.0.0.1:8000/accounts/google/login/
+    template_name = "classlist/google_login.html"
+    return HttpResponseRedirect("/accounts/google/login")
+    # model = User # need to make a user model
+    
+    # options for login page
+    # return HttpResponse("This is the login page!")
+
+    # context = {
+    #     'user' : user,
+    # }
+    
+    # return render(request, 'google_login.html', context)
+
