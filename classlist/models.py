d215420d6d1cd10d162f8224f477672f6bf70515
megkuo <mlk6une@virginia.edu>
2022-10-16 13:32:30 -0400

    Added Class model according to the SIS API's information

--- classlist/models.py
+++ classlist/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from unittest.util import _MAX_LENGTH
 from django.db import models
 import datetime
@@ -41,9 +42,41 @@ class User(models.Model):
 
 
 class Class(models.Model):
-    Class_title = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    Credits = models.IntegerField()
 
+
+    instructor = {
+            "name": models.CharField(max_length=200, blank=True),
+            "email": models.CharField(max_length=200, blank=True)
+        }
+    
+    course_number = models.IntegerField(default=0) # ex. 16351
+    semester_code = models.IntegerField(default=0) # ex. 1228
+    course_section = models.IntegerField(default=0) # 001
+    subject = models.CharField(max_length=20, blank=True) # CS
+    catalog_number = models.IntegerField(blank=True, default=0) # 1010
+    description = models.CharField(max_length=200, blank=True) # Introduction to Information Technology,
+    units = models.IntegerField(default=0) # 3, number of credits
+    component = models.CharField(max_length=20, blank=True) # LEC,
+    class_capacity = models.IntegerField(default=0) # 75,
+    wait_list = models.IntegerField(default=0) # 0
+    wait_cap = models.IntegerField(default=0) # 199,
+    enrollment_total = models.IntegerField(default=0) # 72,
+    enrollment_available = models.IntegerField(default=0) # 3
+    topic = models.CharField(max_length=200, blank=True) # optional description
+    meetings = [
+        {
+            "days": models.CharField(blank=True), # MoWe,
+            "start_time": models.CharField(blank=True), # 17.00.00.000000-05:00,
+            "end_time": models.CharField(blank=True), # 18.15.00.000000-05:00,
+            "facility_description": models.CharField(blank=True), #Olsson Hall 009
+        }
+    ]
+
+    # class_title = subject + " " + catalog_number + ": " + description
+    
+    # description = models.CharField(max_length=200)
+    # Credits = models.IntegerField()
     def __str__(self):
-        return self.Class_title
+        return self.subject + " " + self.catalog_number + ": " + self.description
+
+    
