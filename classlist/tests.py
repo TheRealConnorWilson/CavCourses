0a218c37144e2470c0a98acd117575459588cd3c
nathan hersel <nth5pdk@virginia.edu>
2022-10-23 15:07:49 -0400

    Final tests for sprint 4

--- classlist/tests.py
+++ classlist/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from .models import User, Course, Department, Instructor, Section
 from django.utils import timezone
 from django.urls import reverse
@@ -10,7 +10,7 @@ class DummyTestCase(TestCase):
     def setUp(self):
         x = 1
         y = 2
-    
+
     def test_dummy_test_case(self):
         self.assertEqual(1, 1)
 
@@ -27,34 +27,6 @@ class UserModelTests(TestCase):
 #     last_updated = timezone.now()
 #     return Course.objects.create(question_text=class_text, last_updated=last_updated)
 
-
-# class ClassModelTests(TestCase):
-#     """
-#     Add tests for checking class capacity, days of the week, etc.
-#     Maybe also class compatibility?
-#     """
-
-
-#     def test_no_questions(self):
-#         """
-#         If no questions exist, an appropriate message is displayed.
-#         """
-#         response = self.client.get(reverse('polls:index'))
-#         self.assertEqual(response.status_code, 200)
-#         self.assertContains(response, "No polls are available.")
-#         self.assertQuerysetEqual(response.context['latest_question_list'], [])
-
-    # def test_past_question(self):
-    #     """
-    #     Questions with a pub_date in the past are displayed on the
-    #     index page.
-    #     """
-    #     question = create_question(question_text="Past question.", days=-30)
-    #     response = self.client.get(reverse('polls:index'))
-    #     self.assertQuerysetEqual(
-    #         response.context['latest_question_list'],
-    #         [question],
-    #     )
 
 def create_user(username, first_name, last_name, email):
     user = User()
@@ -96,7 +68,7 @@ class GoogleLoginViewTests(TestCase):    # Still working on the google login tes
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'classlist/google_login.html')
 
-    def test_login_success(self):  # not fully working yet but should be close
+    def test_login_success(self):  # In progress
         self.login_url = reverse('view_name')
         self.register_url = reverse('home')
         self.user = {'email': 'email@gmail.com', 'username': 'username', 'password':'password','password2':'password',
@@ -108,7 +80,7 @@ class GoogleLoginViewTests(TestCase):    # Still working on the google login tes
         response = self.client.post(self.login_url, self.user, format='text/html')
         # self.assertEqual(response.status_code, 302)
 
-    def test_cantlogin_with_no_username(self):    # not working
+    def test_cantlogin_with_no_username(self):    # in progress
         self.login_url = reverse('view_name')
         response = self.client.post(self.login_url, {'password': 'password'}, format='text/html')
         # self.assertEqual(response.status_code, 401)
@@ -127,13 +99,9 @@ class GoogleLoginViewTests(TestCase):    # Still working on the google login tes
 # class testing
 
 # create new class
-def new_course(semester_code, title, units):
-    course = Course()
-    course.semester_code = semester_code
-    course.title = title
-    #course.description = description
-    course.units = units
-    return course
+def new_course(semester_code, title, units, subject, cat_num):
+    return Course.objects.create(catalog_number=cat_num, semester_code=semester_code, title=title,
+                                 description=title, units=units, subject=subject)
 
 
 class CourseTesting(TestCase):  # working
@@ -141,10 +109,15 @@ class CourseTesting(TestCase):  # working
         response = self.client.get(reverse('list'))
         self.assertEqual(response.status_code, 200)
 
-    def test_new_class(self):
+    def test_link(self):  # working
+        response = self.client.get('/list/CS/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_class(self):  # working
         response = self.client.get(reverse('list'))
-        course = new_course(1228, 'Introduction to Information Technology', 3)
-        # self.assertQuerysetEqual(response.context['all_courses'], [course])   # this is not fully working
+        course = Course()
+        course.save()
+        self.assertEqual(len(Course.objects.all()), 1)
 
     def test_adding_class(self):  # working
         response = self.client.get(reverse('list'))
@@ -154,7 +127,15 @@ class CourseTesting(TestCase):  # working
         course.description = 'Introduction to Information Technology'
         course.units = 3
         course.subject = 'CS'
+        course.save()
         self.assertEqual(response.status_code, 200)  # Successfully added class
 
-
-
+    def test_right_num_classes(self):   # working
+        # response = self.client.get(reverse('list'))
+        c = Course(catalog_number=1119, semester_code=1228, title='CS 1119', description='description', units=3,
+                   subject='CS')
+        c.save()
+        c2 = Course(catalog_number=9163, semester_code=132, title='CS 1118', description='description 2', units=3,
+                    subject='CSS')
+        c2.save()
+        self.assertEqual(len(Course.objects.all()), 2)
